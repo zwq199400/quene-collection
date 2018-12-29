2e7d9bc10d42931a1335049c0e92502bcfd0ddb9
zhouwq <zwq19940930@qq.com>
2018-12-29 10:01:19 +0800

    kafka,ActiveMQ 测试demo实现

---
+++ quene/src/main/java/com/example/quene/QueneApplication.java
@@ -0,0 +1,14 @@
+package com.example.quene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueneApplication.class, args);
+    }
+
+}
+
---
+++ quene/src/main/java/com/example/quene/kafka/Receiver.java
@@ -0,0 +1,18 @@
+package com.example.quene.kafka;
+
+import com.example.quene.entity.Message;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author zwq
+ * @date 2018/12/28 16:41
+ */
+@Component
+public class Receiver {
+
+    @KafkaListener(topics = "test1")
+    public void processMessage(String content) {
+        System.out.println(content);
+    }
+}
